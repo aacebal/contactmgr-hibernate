65062e06f9614196c13cec695c8bd2ab0eb15a6c
Adel Acebal <adelacebal@yahoo.com>
2017-09-21 11:27:01 -0400

    saving and retrieving contacts

--- src/main/java/com/adelacebal/contactmgr/Application.java
+++ src/main/java/com/adelacebal/contactmgr/Application.java
@@ -1,15 +1,19 @@
 package com.adelacebal.contactmgr;
 
 import com.adelacebal.contactmgr.model.Contact;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.service.ServiceRegistry;
 
+import java.util.List;
+
 
 public class Application {
     //Hold a reusable reference to a SessionFactory
-//    public static final SessionFactory sessionFactory = buildSessionFactory();
+    public static final SessionFactory sessionFactory = buildSessionFactory();
 
     public static SessionFactory buildSessionFactory() {
         //Create a StandardServiceRegistry
@@ -22,6 +26,47 @@ public class Application {
                 .withEmail("aacebal@me.com")
                 .withPhone(7865553333L)
                 .build();
-        System.out.println(contact);
+
+        save(contact);
+
+        // Display a list of contacts
+        for(Contact c : fetchAllContacts()) {
+            System.out.println(c);
+        }
+
+    }
+
+    @SuppressWarnings("unchecked")
+    private static List<Contact> fetchAllContacts() {
+        // Open a session
+        Session session = sessionFactory.openSession();
+
+        // Create Criteria
+        Criteria criteria = session.createCriteria(Contact.class);
+
+        // Get a list of contact objects according to criteria object
+        List<Contact> contacts = criteria.list();
+
+        //Close session
+        session.close();
+
+        return contacts;
+    }
+
+    private static void save(Contact contact) {
+        // Open a session
+        Session session = sessionFactory.openSession();
+
+        // Begin a transaction
+        session.beginTransaction();
+
+        // Use the section to save the contact
+        session.save(contact);
+
+        //Commit the transaction
+        session.getTransaction().commit();
+
+        // Close the session
+        session.close();
     }
 }
