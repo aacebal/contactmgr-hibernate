8b79ef8ad46cdf6961e9fb4a17e15f0c897c2f50
Adel Acebal <adelacebal@yahoo.com>
2017-09-21 10:40:58 -0400

    configuring h2

---
+++ src/main/java/com/adelacebal/contactmgr/Application.java
@@ -0,0 +1,7 @@
+package com.adelacebal.contactmgr;
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/adelacebal/contactmgr/model/Contact.java
@@ -0,0 +1,65 @@
+package com.adelacebal.contactmgr.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Contact {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Long phone;
+
+    public Contact() { }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phone=" + phone +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Long phone) {
+        this.phone = phone;
+    }
+}
